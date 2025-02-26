502bb2f6b92c2b672839ef07f20e681a24792a35
Your Name <you@example.com>
2025-02-26 22:30:02 +0000

    fix ddns service not start autom update submodules

--- no-ip.sh
+++ no-ip.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-http://dynupdate.no-ip.com/nic/update
+URLNOIP=http://dynupdate.no-ip.com/nic/update
+`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`^C
+curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/network/interfaces/macs/12:d6:80:78:4f:b7/public-ipv4s && echo ""
 
