597f304e00aae25c16b93ad9ebad73ac8397b378
Luis Delgado <luis.delgado13@inacapmail.cl>
2025-04-01 20:50:02 -0300

    up fix

---
+++ database/caracteristicas.sql
@@ -0,0 +1,7 @@
+INSERT INTO caracteristicas (icono_url, nombre) 
+VALUES
+('/img/estadia.png', 'estadia'),
+('/img/alimentacion.png', 'alimentacion'),
+('/img/paseos.png', 'paseos'),
+('/img/peluqueria.png', 'peluqueria'),
+('/img/entrenamiento.png', 'entrenamiento');
